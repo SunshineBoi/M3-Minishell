2fa452b7f6838336bede56f6516a175a97faf52b
harry <kylow1408@gmail.com>
2026-06-13 20:07:31 +0800

    refactored expand_heredoc_line

--- src/exec/exec_fd.c
+++ src/exec/exec_fd.c
@@ -134,13 +134,36 @@ static char	*parse_heredoc_delimiter(const char *target, int *is_quoted)
 	return (sb.buf);
 }
 
+static void	append_expanded_var(t_strbuf *sb, const char *line, size_t *i, char **envp, int status)
+{
+	char	*status_str;
+	size_t	var_len;
+
+	if (line[*i + 1] == '?')
+	{
+		status_str = itoa_status(status);
+		if (status_str)
+			sb_push_str(sb, status_str);
+		free(status_str);
+		*i += 2;
+	}
+	else if (is_name_start(line[*i + 1]))
+	{
+		var_len = var_name_len(line + *i + 1);
+		sb_push_str(sb, env_lookup(envp, line + *i + 1, var_len));
+		*i += 1 + var_len;
+	}
+	else
+	{
+		sb_push_char(sb, '$');
+		(*i)++;
+	}
+}
+
 static char	*expand_heredoc_line(const char *line, char **envp, int last_status)
 {
 	t_strbuf	sb;
 	size_t		i;
-	size_t		var_len;
-	const char	*val;
-	char		*status_str;
 
 	if (sb_init(&sb) != 0)
 		return (NULL);
@@ -148,31 +171,7 @@ static char	*expand_heredoc_line(const char *line, char **envp, int last_status)
 	while (line[i])
 	{
 		if (line[i] == '$' && line[i + 1])
-		{
-			if (line[i + 1] == '?')
-			{
-				status_str = itoa_status(last_status);
-				if (status_str)
-				{
-					sb_push_str(&sb, status_str);
-					free(status_str);
-				}
-				i += 2;
-			}
-			else if (is_name_start(line[i + 1]))
-			{
-				var_len = var_name_len(line + i + 1);
-				val = env_lookup(envp, line + i + 1, var_len);
-				if (val)
-					sb_push_str(&sb, val);
-				i += 1 + var_len;
-			}
-			else
-			{
-				sb_push_char(&sb, '$');
-				i++;
-			}
-		}
+			append_expanded_var(&sb, line, &i, envp, last_status);
 		else
 		{
 			sb_push_char(&sb, line[i]);
