8d4322a99ea74a6db03bd0dd07bb8e1d32d4ec3e
Low Kai Yuan <lkai-yua@c1r3s3.42singapore.sg>
2026-07-06 17:45:19 +0800

    Norminette

--- include/ast.h
+++ include/ast.h
@@ -19,7 +19,7 @@ typedef enum e_node_type
 {
 	NODE_CMD,
 	NODE_BINOP
-}							t_node_type;
+}								t_node_type;
 
 typedef enum e_binop_type
 {
@@ -27,7 +27,7 @@ typedef enum e_binop_type
 	BIN_AND,
 	BIN_OR,
 	BIN_SEQ
-}							t_binop_type;
+}								t_binop_type;
 
 typedef enum e_redir_type
 {
@@ -35,51 +35,50 @@ typedef enum e_redir_type
 	REDIR_OUT,
 	REDIR_APPEND,
 	REDIR_HEREDOC
-}							t_redir_type;
+}								t_redir_type;
 
 typedef struct s_span
 {
-	int						start;
-	int						end;
-}							t_span;
+	int							start;
+	int							end;
+}								t_span;
 
 typedef struct s_redir
 {
-	t_redir_type			type;
-	char					*target;
-	int						fd;
-	struct s_redir			*next;
-}							t_redir;
+	t_redir_type				type;
+	char						*target;
+	int							fd;
+	struct s_redir				*next;
+}								t_redir;
 
 typedef struct s_cmd_node
 {
-	char					**argv;
-	char					**envp;
-	t_redir					*redirs;
-}							t_cmd_node;
+	char						**argv;
+	char						**envp;
+	t_redir						*redirs;
+}								t_cmd_node;
 
 typedef struct s_ast_node
 {
-	t_node_type				type;
-	t_span					span;
-	union
+	t_node_type					type;
+	t_span						span;
+	union u_ast_content
 	{
-		t_cmd_node			cmd;
-		struct
+		t_cmd_node				cmd;
+		struct s_binop_node
 		{
-			t_binop_type	op;
-			struct s_ast_node
-							*left;
-			struct s_ast_node
-							*right;
-		}					binop;
-	}						content;
-}							t_ast_node;
+			t_binop_type		op;
+			struct s_ast_node	*left;
+			struct s_ast_node	*right;
+		}						binop;
+	}							content;
+}								t_ast_node;
 
-t_ast_node					*ast_new_cmd(char **argv, t_redir *redirs,
-								t_span span);
-t_ast_node					*ast_new_binop(t_binop_type op, t_ast_node *left,
-								t_ast_node *right, t_span span);
-void						ast_free(t_ast_node *node);
+t_ast_node						*ast_new_cmd(char **argv, t_redir *redirs,
+									t_span span);
+t_ast_node						*ast_new_binop(t_binop_type op,
+									t_ast_node *left, t_ast_node *right,
+									t_span span);
+void							ast_free(t_ast_node *node);
 
 #endif
